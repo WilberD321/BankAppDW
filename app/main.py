eaf425dfc78cd229ea1dbb2caadb567f11b1f503
WilberD321 <david.wilber04@gmail.com>
2026-08-12 12:18:01 -0400

    added frontend functions

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.controllers import accountsControllers, customersControllers, transactionsControllers
 
 app = FastAPI(title="BankAppDW API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(customersControllers.router)
 app.include_router(accountsControllers.router)
